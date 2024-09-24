ecd42e4491863183a9a13008e8645b75d8763bd3
zchanway <146023170+zchanway@users.noreply.github.com>
2024-09-23 22:03:15 -0500

    crear joyas

--- src/main/java/practica/manejo/hilo/cibertec/sextociclo_t/controller/JoyaController.java
+++ src/main/java/practica/manejo/hilo/cibertec/sextociclo_t/controller/JoyaController.java
@@ -1,8 +1,11 @@
 package practica.manejo.hilo.cibertec.sextociclo_t.controller;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import practica.manejo.hilo.cibertec.sextociclo_t.service.JoyaService;
+
+import java.io.IOException;
 
 @RestController
 public class JoyaController {
@@ -13,6 +16,10 @@ public class JoyaController {
     public JoyaController(JoyaService joyaService) {
         this.joyaService = joyaService;
     }
+    @GetMapping("/crear-joyas-async")
+    public String crearJoyasAsync() throws InterruptedException, IOException {
 
+
+    return "";}
 }
 
