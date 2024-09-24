5c1d98a9a8cd70bda8cc0f32ef47e655e3ccd9f9
zchanway <146023170+zchanway@users.noreply.github.com>
2024-09-23 21:59:58 -0500

    JoyaController

--- src/main/java/practica/manejo/hilo/cibertec/sextociclo_t/controller/JoyaController.java
+++ src/main/java/practica/manejo/hilo/cibertec/sextociclo_t/controller/JoyaController.java
@@ -1,4 +1,18 @@
 package practica.manejo.hilo.cibertec.sextociclo_t.controller;
 
+
+import org.springframework.web.bind.annotation.RestController;
+import practica.manejo.hilo.cibertec.sextociclo_t.service.JoyaService;
+
+@RestController
 public class JoyaController {
+
+
+    private final JoyaService joyaService;
+
+    public JoyaController(JoyaService joyaService) {
+        this.joyaService = joyaService;
+    }
+
 }
+
