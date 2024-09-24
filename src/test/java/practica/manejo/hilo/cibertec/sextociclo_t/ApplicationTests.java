c9c882b5b76f5e42fad70d92b359dcd8a7702d9a
Melys02 <melys0919@gmail.com>
2024-09-23 19:12:07 -0500

    inicio de proyecto

---
+++ src/test/java/practica/manejo/hilo/cibertec/sextociclo_t/ApplicationTests.java
@@ -0,0 +1,13 @@
+package practica.manejo.hilo.cibertec.sextociclo_t;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
